b529a5573c6688d2815a3e2989cd1b3e20a53049
unknown <evgeniy.barabanov.96@mail.ru>
2023-01-16 20:09:18 +0300

    initial test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import Header from "./Components/Header/Header.jsx"
+import Main from "./Components/Main/Main.jsx"
+import Footer from "./Components/Footer/Footer.jsx"
 function App(){
     return(
         <div className="App">
-            <h1>hello world</h1>
+            <Header />
+            <Main/>
+            <Footer />
         </div>
     )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Nav from "../Nav/Nav.jsx"
+
+function Header(){
+    return(
+        <div className="Header">
+            <h1>Header</h1>
+            <Nav />
+        </div>
+    )
+}
+
+export default Header;
