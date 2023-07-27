c2dec6c363cb3852481c655eeecfa9dcaee8db93
EvgeniyBarabanov <evgeniy.barabanov.96@mail.ru>
2023-07-27 10:10:06 +0300

    sprintGame output word works

--- src/Components/Sprint/SprintGame.jsx
+++ src/Components/Sprint/SprintGame.jsx
@@ -4,16 +4,16 @@ import { words } from 'popular-english-words';
 
 function SprintGame(){
     
-    const [wordRu, setWordRu] = useState({})
+    const [word, setWord] = useState({})
 
     useEffect(()=>{
-        getWord(params.level)
+        getWord(params.level, getRandomFlag(0,1))
     },[])
 
     const params = useParams()
     const popularWords = words.getMostPopular(10000)
     
-    const getWord = function(lvl){
+    const getWord = function(lvl, positive){
 
         const groupWords = {
             'A1':[0, 1666],
@@ -28,54 +28,53 @@ function SprintGame(){
         const wordEngRight = (allWords[Math.floor(Math.random() * (allWords.length-1 - 0 + 1) ) + 0])
         const wordEngWrong = (allWords[Math.floor(Math.random() * (allWords.length-1 - 0 + 1) ) + 0])
 
-        Promise.all([
-            fetch('http://tmp.myitschool.org/API/translate/?source=en&target=ru&word=' + wordEngWrong)
-            .then(response => response.json())
-            .then(wrong => {
-                return wrong
-            }),
+        if (positive){
             fetch('http://tmp.myitschool.org/API/translate/?source=en&target=ru&word=' + wordEngRight)
             .then(response => response.json())
             .then(result => {
-                return result
+                result.flag = true;
+                setWord({...result})
             })
-        ]).then(values => {
-            values[1].falseTranslate = values[0].translate
-            values[1].output =  Object.values(values[1])[getRandomIntInclusive(1,2)]
-            setWordRu({...values[1]})
-        })
-
+        }else{
+            fetch('http://tmp.myitschool.org/API/translate/?source=en&target=ru&word=' + wordEngWrong)
+            .then(response => response.json())
+            .then(result => {
+                result.flag = false;
+                result.word = wordEngRight
+                setWord({...result})
+            })
+        }
     } 
     
-    const getRandomIntInclusive = function(min, max) {
+    const getRandomFlag = function(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min
+        if (Math.floor(Math.random() * (max - min + 1)) + min == 0){
+            return true
+        }else{
+            return false
+        }
     }
 
-    const test = function(value){ 
-
-        if ((wordRu.translate == wordRu.output) && value == "true"){
-            console.log('you right');
-        }else if ((wordRu.translate !== wordRu.output) && value == "false"){
-            console.log('you right');
+    const test = function(value){
+        if(value === word.flag){
+            console.log('труе');
         }else{
-            console.log('you dont right');
+            console.log('фалсе');
         }
-
-        getWord(params.level)
+        getWord(params.level, getRandomFlag(0,1))
     }
 
     return(
         <div className="sprintgame">
-        {wordRu &&
-            <div>
-                <h1>{wordRu.word}</h1>
-                <h1>{wordRu.output}</h1>
-            </div>
-        }
-        <button onClick={()=>test('true')}>right</button>
-        <button onClick={()=>test('false')}>wrong</button>
+            {word &&
+                <div>
+                    <h1>{word.word}</h1>
+                    <h1>{word.translate}</h1>
+                </div>
+            }
+            <button onClick={()=>test(true)}>right</button>
+            <button onClick={()=>test(false)}>wrong</button>
         </div>
     )
 }
